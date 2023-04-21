3798fa6c4534a0e12c1b72ef88257606ddcebb14
id2k1149 <id2k1149@gmail.com>
2023-04-21 12:03:14 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Max Franz Immelmann on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
