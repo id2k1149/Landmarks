c8aca70b16b5b650d8db4b849a51a1cf4398464e
id2k1149 <id2k1149@gmail.com>
2023-04-23 09:04:25 -0400

    Add a starting point to the path, assuming a container with size 100 x 100 px.

--- Landmarks/Views/BadgeBackground.swift
+++ Landmarks/Views/BadgeBackground.swift
@@ -10,9 +10,16 @@ import SwiftUI
 struct BadgeBackground: View {
     var body: some View {
         Path { path in
-
-                }
-                .fill(.black)
+            var width: CGFloat = 100.0
+            let height = width
+            path.move(
+                to: CGPoint(
+                    x: width * 0.95,
+                    y: height * 0.20
+                )
+            )
+        }
+        .fill(.black)
     }
 }
 
