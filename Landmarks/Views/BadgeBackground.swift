280cfde5e5a17f5cb46522ec38a78bc57865b7aa
id2k1149 <id2k1149@gmail.com>
2023-04-23 09:03:24 -0400

    Create struct BadgeBackground

---
+++ Landmarks/Views/BadgeBackground.swift
@@ -0,0 +1,23 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by Max Franz Immelmann on 4/23/23.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Path { path in
+
+                }
+                .fill(.black)
+    }
+}
+
+struct BadgeBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeBackground()
+    }
+}
