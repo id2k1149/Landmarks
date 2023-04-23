d08c57a8e5785de749bf73384e47755db7c756bb
id2k1149 <id2k1149@gmail.com>
2023-04-23 08:56:45 -0400

    create a structure called HexagonParameters.

---
+++ Landmarks/Views/HexagonParameters.swift
@@ -0,0 +1,16 @@
+//
+//  HexagonParameters.swift
+//  Landmarks
+//
+//  Created by Max Franz Immelmann on 4/23/23.
+//
+
+import Foundation
+
+struct HexagonParameters {
+    struct Segment {
+        let line: CGPoint
+        let curve: CGPoint
+        let control: CGPoint
+    }
+}
