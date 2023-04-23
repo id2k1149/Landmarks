c3daab91a00d9ffedad073af6d4dc4538f7b7057
id2k1149 <id2k1149@gmail.com>
2023-04-22 21:21:49 -0400

    Create a new view called FavoriteButton.swift.

---
+++ Landmarks/Views/FavoriteButton.swift
@@ -0,0 +1,20 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Max Franz Immelmann on 4/22/23.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton()
+    }
+}
