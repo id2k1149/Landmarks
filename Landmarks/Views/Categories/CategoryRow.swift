2872e048de0c38d49da0623d842c65381665f547
id2k1149 <id2k1149@gmail.com>
2023-04-23 10:24:04 -0400

    Add properties for the category name and the list of items in that category.

---
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Max Franz Immelmann on 4/23/23.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+    
+    static var previews: some View {
+        CategoryRow(
+            categoryName: landmarks[0].category.rawValue,
+            items: Array(landmarks.prefix(3))
+        )
+    }
+}
