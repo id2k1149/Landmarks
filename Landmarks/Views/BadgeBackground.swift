c85ea8b2b6acae834013bca92f9efb6de11a5c65
id2k1149 <id2k1149@gmail.com>
2023-04-23 09:21:15 -0400

    Replace the solid black background with a gradient to match the design.

--- Landmarks/Views/BadgeBackground.swift
+++ Landmarks/Views/BadgeBackground.swift
@@ -14,8 +14,8 @@ struct BadgeBackground: View {
                 var width: CGFloat = min(geometry.size.width, geometry.size.height)
                 let height = width
                 let xScale: CGFloat = 0.832
-                                let xOffset = (width * (1.0 - xScale)) / 2.0
-                                width *= xScale
+                let xOffset = (width * (1.0 - xScale)) / 2.0
+                width *= xScale
                 path.move(
                     to: CGPoint(
                         x: width * 0.95 + xOffset,
@@ -43,10 +43,18 @@ struct BadgeBackground: View {
                     )
                 }
             }
-            .fill(.black)
+            .fill(.linearGradient(
+                Gradient(colors: [Self.gradientStart, Self.gradientEnd]),
+                startPoint: UnitPoint(x: 0.5, y: 0),
+                endPoint: UnitPoint(x: 0.5, y: 0.6)
+            ))
         }
     }
+    static let gradientStart = Color(red: 239.0 / 255, green: 120.0 / 255, blue: 221.0 / 255)
+    static let gradientEnd = Color(red: 239.0 / 255, green: 172.0 / 255, blue: 120.0 / 255)
 }
+
+
 
 struct BadgeBackground_Previews: PreviewProvider {
     static var previews: some View {
