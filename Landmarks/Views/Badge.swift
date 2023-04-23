79014c95430d3aa8da8062e5d518b9f3624bf13d
id2k1149 <id2k1149@gmail.com>
2023-04-23 09:32:39 -0400

    Create a new SwiftUI view called Badge.

---
+++ Landmarks/Views/Badge.swift
@@ -0,0 +1,20 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Max Franz Immelmann on 4/23/23.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        BadgeBackground()
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
