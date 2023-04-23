53d966b7b74e55fd96b2564d1f6acdba43160e87
id2k1149 <id2k1149@gmail.com>
2023-04-22 20:58:57 -0400

    add var filteredLandmarks

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -10,6 +10,12 @@ import SwiftUI
 struct LandmarkList: View {
     @State private var showFavoritesOnly = false
     
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationView  {
             List(landmarks) { landmark in
