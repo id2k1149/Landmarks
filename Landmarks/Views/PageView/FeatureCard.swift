626dd89a38563f62f4f514aca0d216a65485100a
id2k1149 <id2k1149@gmail.com>
2023-04-24 09:37:06 -0400

    Add a new SwiftUI view file, named FeatureCard.swift that displays the landmark’s feature image.

---
+++ Landmarks/Views/PageView/FeatureCard.swift
@@ -0,0 +1,24 @@
+//
+//  FeatureCard.swift
+//  Landmarks
+//
+//  Created by Max Franz Immelmann on 4/24/23.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    var landmark: Landmark
+
+    var body: some View {
+        landmark.featureImage?
+            .resizable()
+            .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
+
+struct FeatureCard_Previews: PreviewProvider {
+    static var previews: some View {
+        FeatureCard(landmark: ModelData().features[0])
+    }
+}
