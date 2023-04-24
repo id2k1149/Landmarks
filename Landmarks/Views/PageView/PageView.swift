15066ab34f125c6d60bcea87b8282b1580495390
id2k1149 <id2k1149@gmail.com>
2023-04-24 09:40:58 -0400

    Update the preview provider to pass the required array of views, and the preview starts working.

--- Landmarks/Views/PageView/PageView.swift
+++ Landmarks/Views/PageView/PageView.swift
@@ -17,6 +17,7 @@ struct PageView<Page: View>: View {
 
 struct PageView_Previews: PreviewProvider {
     static var previews: some View {
-        PageView()
+        PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+            .aspectRatio(3 / 2, contentMode: .fit)
     }
 }
