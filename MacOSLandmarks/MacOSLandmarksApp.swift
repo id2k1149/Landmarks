fed5c1fd61fa289dc9ac51cd22d645d49f29fd92
id2k1149 <id2k1149@gmail.com>
2023-04-24 15:28:43 -0400

    add MacOSLandmarks

---
+++ MacOSLandmarks/MacOSLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOSLandmarksApp.swift
+//  MacOSLandmarks
+//
+//  Created by Max Franz Immelmann on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct MacOSLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
