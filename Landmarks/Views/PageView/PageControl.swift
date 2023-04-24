07d441e6abf6602630b9acf67147992eb518a64a
id2k1149 <id2k1149@gmail.com>
2023-04-24 10:00:15 -0400

    Create a new SwiftUI view file, named PageControl.swift. Update the PageControl type to conform to the UIViewRepresentable protocol.

---
+++ Landmarks/Views/PageView/PageControl.swift
@@ -0,0 +1,25 @@
+//
+//  PageControl.swift
+//  Landmarks
+//
+//  Created by Max Franz Immelmann on 4/24/23.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageControl: UIViewRepresentable {
+    var numberOfPages: Int
+    @Binding var currentPage: Int
+
+    func makeUIView(context: Context) -> UIPageControl {
+        let control = UIPageControl()
+        control.numberOfPages = numberOfPages
+
+        return control
+    }
+
+    func updateUIView(_ uiView: UIPageControl, context: Context) {
+        uiView.currentPage = currentPage
+    }
+}
