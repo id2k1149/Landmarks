aa5536cd6e87f8e2e255c39163d57c47428df708
id2k1149 <id2k1149@gmail.com>
2023-04-24 12:16:17 -0400

    in NotificationController add the landmark, title, and message properties.

---
+++ WatchLandmarks Watch App/NotificationController.swift
@@ -0,0 +1,36 @@
+//
+//  NotificationController.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Max Franz Immelmann on 4/24/23.
+//
+
+import WatchKit
+import SwiftUI
+import UserNotifications
+
+class NotificationController: WKUserNotificationHostingController<NotificationView> {
+    var landmark: Landmark?
+    var title: String?
+    var message: String?
+    
+    override var body: NotificationView {
+        NotificationView()
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+    override func didReceive(_ notification: UNNotification) {
+        // This method is called when a notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+    }
+}
