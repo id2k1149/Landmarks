9eca776649f83e8c70dfe1c3751c80710a8b0ac0
id2k1149 <id2k1149@gmail.com>
2023-04-24 11:47:58 -0400

    Return a CircleImage view from the body() method.

--- WatchLandmarks Watch App/LandmarkDetail.swift
+++ WatchLandmarks Watch App/LandmarkDetail.swift
@@ -16,7 +16,8 @@ struct LandmarkDetail: View {
     }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        CircleImage(image: landmark.image.resizable())
+            .scaledToFill()
     }
 }
 
