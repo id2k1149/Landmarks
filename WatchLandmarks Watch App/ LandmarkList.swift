3aa2970190d5cc5bc5c3325c998657c2f7dfc464
id2k1149 <id2k1149@gmail.com>
2023-04-24 20:25:50 -0400

    add an #if condition to prevent the secondary text from appearing in a watchOS build.

---
+++ WatchLandmarks Watch App/ LandmarkList.swift
@@ -0,0 +1,55 @@
+//
+//   LandmarkList.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Max Franz Immelmann on 4/24/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter  { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView  {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .environmentObject(ModelData())
+        /*
+        ForEach(
+            ["iPhone SE (3rd generation)",
+             "iPhone 14 Pro"],
+            id: \.self) { deviceName in
+                LandmarkList()
+                    .previewDevice(PreviewDevice(rawValue: deviceName))
+                    .previewDisplayName(deviceName)
+        }
+         */
+    }
+}
