14b8cae3b44f328353cab48312de354656733e03
id2k1149 <id2k1149@gmail.com>
2023-04-24 21:07:29 -0400

    add a LandmarkCommands structure that conforms to the Commands protocol, with a computed body property.

---
+++ MacOSLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by Max Franz Immelmann on 4/24/23.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        
+    }
+}
