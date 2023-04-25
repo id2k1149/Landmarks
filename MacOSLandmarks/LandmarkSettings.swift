231c41ab8ff8278a2c29190c9ddbf99e49680c4d
id2k1149 <id2k1149@gmail.com>
2023-04-24 21:47:45 -0400

    Create a new SwiftUI view called LandmarkSettings that targets only the macOS app.

---
+++ MacOSLandmarks/LandmarkSettings.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkSettings.swift
+//  MacOSLandmarks
+//
+//  Created by Max Franz Immelmann on 4/24/23.
+//
+
+import SwiftUI
+
+struct LandmarkSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkSettings_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkSettings()
+    }
+}
