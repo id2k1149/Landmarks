1e6a8507403675afc6d2679c59ca6a6aef221de6
id2k1149 <id2k1149@gmail.com>
2023-04-24 21:42:49 -0400

    Change the longitude and latitude delta used to construct the region property to a value that depends on zoom.

--- Landmarks/Views/Helpers/MapView.swift
+++ Landmarks/Views/Helpers/MapView.swift
@@ -16,14 +16,22 @@ struct MapView: View {
     private var zoom: Zoom = .medium
     
     enum Zoom: String, CaseIterable, Identifiable {
-           case near = "Near"
-           case medium = "Medium"
-           case far = "Far"
-
-           var id: Zoom {
-               return self
-           }
-       }
+        case near = "Near"
+        case medium = "Medium"
+        case far = "Far"
+        
+        var id: Zoom {
+            return self
+        }
+    }
+    
+    var delta: CLLocationDegrees {
+        switch zoom {
+        case .near: return 0.02
+        case .medium: return 0.2
+        case .far: return 2
+        }
+    }
     
     var body: some View {
         Map(coordinateRegion: $region)
@@ -35,7 +43,7 @@ struct MapView: View {
     private func setRegion(_ coordinate: CLLocationCoordinate2D) {
         region = MKCoordinateRegion(
             center: coordinate,
-            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+            span: MKCoordinateSpan(latitudeDelta: delta, longitudeDelta: delta)
         )
     }
 }
